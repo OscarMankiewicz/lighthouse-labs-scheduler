ad0032b7892a8540e09748fcd981c8b85be1ffd7
Oscar <omankiewicz77@gmail.com>
2023-07-07 14:48:52 -0400

    removed unused code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay} from "../helpers/selectors";
+import {getInterview} from "../helpers/selectors";
 import useApplicationData from "../hooks/useApplicationData";
 
 export default function Application(props) {
