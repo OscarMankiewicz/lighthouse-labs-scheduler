e9fe8c0e3b8ca4c43a0d157fd39e4d22e8c65bf9
Oscar <omankiewicz77@gmail.com>
2023-07-07 14:47:55 -0400

    added an onClick to make story work

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -23,7 +23,7 @@ export default function Show (props) {
                         className="appointment__actions-button"
                         src="images/trash.png"
                         alt="Delete"
-                        onClick={deleteAppointment}
+                        onClick={props.onDelete || deleteAppointment}
                     />
                 </section>
             </section>
